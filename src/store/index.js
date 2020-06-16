e18b662ce06869b1df4957e1e7732c84024697e2
TheTechdoodle <admin+github@darkender.com>
2020-06-16 12:36:37 -0700

    add basic user sorting

--- src/store/index.js
+++ src/store/index.js
@@ -5,19 +5,47 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        users: []
+        users: [],
+        weights: {
+            projectsCount: 1.0,
+            followerCount: 0.8,
+            achievementsCount: 0.5,
+            skillCount: 0.6,
+            interestCount: 0.0,
+            introLength: 0.0
+        }
     },
     mutations: {
         setUsers(state, users)
         {
             state.users = users;
+        },
+        setWeight(state, {name, value})
+        {
+            state.weights[name] = value;
         }
     },
     actions: {},
     modules: {},
     getters: {
-        usersReady: state => {
+        usersReady: state =>
+        {
             return state.users.length > 0;
+        },
+        usersWeighted: state =>
+        {
+            return state.users.map(user =>
+            {
+                let score = 0.0;
+                score += state.weights.achievementsCount * user.achievements;
+                score += state.weights.followerCount * user.followers;
+                score += state.weights.projectsCount * user.projects;
+                score += state.weights.interestCount * user.interests.length;
+                score += state.weights.skillCount * user.skills.length;
+                score += state.weights.introLength * user.intro.length;
+                
+                return {user, score};
+            });
         }
     }
 });
